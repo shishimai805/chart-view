8f7bfd2056ea3f1d795536e5090cecc7cee452d1
shishimai805 <88955687+shishimai805@users.noreply.github.com>
2023-12-26 16:48:45 +0900

    Update settings.py

--- chartview/settings.py
+++ chartview/settings.py
@@ -125,10 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #STATIC_URL = 'static/'
-
-STATIC_URL = "static/"
-STATIC_ROOT = str(BASE_DIR / "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#パターンA
+#STATIC_URL = "static/"
+#STATIC_ROOT = str(BASE_DIR / "staticfiles")
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#パターンB
+STATIC_URL = '/staticfile/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
